0144208d935e2cc0fe60660fc625c105c1f858ae
fahim2000-f <mohammedaminefahim567@gmail.com>
2022-07-06 19:44:51 +0100

    all of commit

---
+++ portfolio.js
@@ -0,0 +1,76 @@
+window.addEventListener("load", () => {
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".portfolio-section").classList.add("active");
+});
+
+// toggle navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+//active section
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
+// portfolio details popup
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    toggleportfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+function toggleportfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("fade-out");
+}
+document
+  .querySelector(".pp-close")
+  .addEventListener("click", toggleportfolioPopup);
+
+//hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("pp-inner")) {
+    toggleportfolioPopup();
+  }
+});
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(
+    ".portfolio-item-thumbnail img"
+  ).src;
+  document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(
+    ".portfolio-item-details"
+  ).innerHTML;
+}
